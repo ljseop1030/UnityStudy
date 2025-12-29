3aa8a55e0cfbf3871b6ef82af4b2e9c071e09a24
Jiseop Lee <ljseop1030@gmail.com>
2025-12-29 17:21:50 +0900

    open script in VS code and write comments

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // 프로젝트 실행되자마자 시작됨. 오브젝트 호출 안돼도. 1번만(초기 설정)
     void Start()
     {
         
     }
 
     // Update is called once per frame
+    // 매 프레임마다 실행. 움직임, 입력체크 등에 사용.
     void Update()
     {
-        
+        // Move vehicle foward
     }
 }
