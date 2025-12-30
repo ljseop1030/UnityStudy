07d5411e8afcfb5c987ac891ad43f5d8e37ee7b6
Jiseop Lee <ljseop1030@gmail.com>
2025-12-30 10:59:23 +0900

    [1.3.3] modify: camera angle

--- Prototype1/Assets/Scripts/FollowPlayer.cs
+++ Prototype1/Assets/Scripts/FollowPlayer.cs
@@ -14,6 +14,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;        
+        transform.position = player.transform.position + new Vector3(0,5,-8);        
+        // player의 위치에 (0,5,-8) 더해서 카메라 위치 지정. 같은 3차원 vector로 만들어줘야 함.
     }
 }
