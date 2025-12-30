6485303aa897db89c11d3be783b9e35d8f511891
Jiseop Lee <ljseop1030@gmail.com>
2025-12-30 11:03:22 +0900

    [1.3.4] modify: make angle offset into public variable

--- Prototype1/Assets/Scripts/FollowPlayer.cs
+++ Prototype1/Assets/Scripts/FollowPlayer.cs
@@ -5,6 +5,8 @@ public class FollowPlayer : MonoBehaviour
 
     public GameObject player;
 
+    public Vector3 offset = new Vector3(0,5,-8); // 벡터는 new Vector3로 만들어줘야함. 클래스인가봄.
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,7 +16,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0,5,-8);        
+        transform.position = player.transform.position + offset;        
         // player의 위치에 (0,5,-8) 더해서 카메라 위치 지정. 같은 3차원 vector로 만들어줘야 함.
     }
 }
