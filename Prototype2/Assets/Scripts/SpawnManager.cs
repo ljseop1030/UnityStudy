57e4e75acb194d7143493d1f6595996907f70644
Jiseop Lee <ljseop1030@gmail.com>
2025-12-31 12:02:03 +0900

    [2.4.2] add: InvokeRepeating

--- Prototype2/Assets/Scripts/SpawnManager.cs
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -7,29 +7,34 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
 
+    private float startDelay  =2;
+    private float spawnInterval = 1.5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        // InvokeRepeating ( 함수명 , 처음 호출까지 기다리는 시간, 반복 간격)
+        // 어떤 함수를 반복 호출해주는 기능
+        // start에 작성함
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            SpawnRandomAnimal(); 
-        }
+
     }
 
     void SpawnRandomAnimal()
     {
         int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0,spawnPosZ);
 
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-            // Instantiate (복제할 오브젝트, 위치, 생성 시 회전값 (원래 프리팹의 회전값 호출한거임!) )
-            // 회전 안 넣을 거면 Quaternion.identity 하면 됨   
-        }
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        // Instantiate (복제할 오브젝트, 위치, 생성 시 회전값 (원래 프리팹의 회전값 호출한거임!) )
+        // 회전 안 넣을 거면 Quaternion.identity 하면 됨   
+    
     }
 }
