3b265a00b785b968be36960ce8b9a095d5e66334
Jiseop Lee <ljseop1030@gmail.com>
2025-12-31 11:41:30 +0900

    [2.3.4] fix: ;

--- Prototype2/Assets/Scripts/SpawnManager.cs
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
 
     private float spawnRangeX = 20;
-    private float spawnPosZ = 20
+    private float spawnPosZ = 20;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
